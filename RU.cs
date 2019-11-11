93958165e3c1faaa88326b33ff6a75efea62b581
Liza-Horn <56540405+Liza-Horn@users.noreply.github.com>
2019-11-11 09:51:15 +0400

    Костюк Горн2

---
+++ RU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+                Console.WriteLine("r = ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            if (r < 20)
+                Console.WriteLine("Неудолитворяет условие");
+            else Console.WriteLine((Math.PI * r * r) - (Math.PI * 20 * 20));
+
+
+            Console.ReadKey();
+
+           
+        }
+    }
+}
